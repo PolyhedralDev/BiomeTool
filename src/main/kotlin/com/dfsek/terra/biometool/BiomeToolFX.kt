65c8553444ef4f7e2f6af0ecb32d1577fd36a4f7
dfsek <dfsek@protonmail.com>
2021-11-30 16:01:50 -0700

    cleanup

--- src/main/kotlin/com/dfsek/terra/biometool/BiomeToolFX.kt
+++ src/main/kotlin/com/dfsek/terra/biometool/BiomeToolFX.kt
@@ -4,9 +4,7 @@ import javafx.application.Application.launch
 import tornadofx.App
 
 
-class BiomeToolFX : App(BiomeToolView::class) {
-
-}
+class BiomeToolFX : App(BiomeToolView::class)
 
 fun main() {
     launch(BiomeToolFX::class.java, "")
