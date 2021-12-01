270239c787cf40f54f8e19daa898036a4512752d
solonovamax <solonovamax@12oclockpoint.com>
2021-12-01 14:19:08 -0500

    set java version to 17 to fix gradle imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,24 +9,32 @@ version = "0.2.3"
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://repo.codemc.org/repository/maven-public") }
+    maven {
+        name = "CodeMC"
+        url = uri("https://repo.codemc.org/repository/maven-public/")
+    }
+}
+
+java {
+    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-
+    
     implementation("com.dfsek.terra:base:6.0.0-BETA+d0872f42")
     
     implementation("ca.solo-studios:slf4k:0.3.1")
-
+    
     implementation("org.apache.logging.log4j:log4j-api:2.14.1")
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
-
+    
     implementation("com.google.guava:guava:31.0.1-jre")
-
+    
     implementation("no.tornado:tornadofx:1.7.20") {
         exclude("org.jetbrains.kotlin")
     }
