cd94b7f9c1ec74b7be3aea7f9dee6f5c40dfa851
dfsek <dfsek@protonmail.com>
2021-12-09 08:55:36 -0700

    clean up dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,6 @@ dependencies {
 
     implementation("ca.solo-studios:slf4k:0.3.1")
 
-    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
 
     implementation("com.google.guava:guava:31.0.1-jre")
 
